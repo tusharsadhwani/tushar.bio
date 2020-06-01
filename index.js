2b4950767898c493dd00b3c0c31082fc7545d9b9
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-06-01 08:53:39 -0700

    Move data to separate file

---
+++ index.js
@@ -0,0 +1,43 @@
+const data = {
+  tabs: ["About", "Resume"],
+  activeTab: "",
+  skills: [
+    {
+      name: "Python",
+      score: 90,
+    },
+    {
+      name: "Flutter",
+      score: 80,
+    },
+    {
+      name: "JavaScript ES6+",
+      score: 75,
+    },
+    {
+      name: "Flask",
+      score: 70,
+    },
+    {
+      name: "SQL",
+      score: 70,
+    },
+  ],
+  showSkills: false,
+  projects: [
+    {
+      name: "Sudoku",
+      description:
+        "A complete 2-player sudoku game built using Flutter and Firebase.",
+      link: "https://github.com/tusharsadhwani/sudoku-online",
+      image: "assets/images/sudoku.jpg",
+    },
+    {
+      name: "ChatApp",
+      description:
+        "A demo chat application made in flutter, with a Flask and SQLite backend.",
+      link: "https://github.com/tusharsadhwani/chat_app",
+      image: "assets/images/chat.jpg",
+    },
+  ],
+};
